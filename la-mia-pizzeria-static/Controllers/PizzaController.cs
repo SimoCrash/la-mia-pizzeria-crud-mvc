7931aec45990d15f262c696abdbb33af2b771432
SimoCrash <simone.crescenzi@hotmail.com>
2023-04-13 19:35:20 +0200

    Upload creato

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -52,6 +52,46 @@ namespace la_mia_pizzeria_static.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult Update(int id)
+        {
+            using var ctx = new PizzeriaContext();
+            var pizza = ctx.Pizzas.FirstOrDefault(p => p.Id == id);
+
+            if(pizza == null)
+            {
+                return View($"Non è stato trovato l'id n° {id}");
+            }
+            return View(pizza);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+
+            if(!ModelState.IsValid)
+            { 
+                return View(pizza); 
+            }
+
+            using var ctx = new PizzeriaContext();
+            var pizzaToUpdate = ctx.Pizzas.FirstOrDefault(p => p.Id == id);
+
+            if(pizzaToUpdate == null)
+            {
+                return View($"Non è stato trovato l'id n° {id}");
+            }
+
+            pizzaToUpdate.Nome = pizza.Nome;
+            pizzaToUpdate.Descrizione = pizza.Descrizione;
+            pizzaToUpdate.Foto = pizza.Foto;    
+            pizzaToUpdate.Prezzo = pizza.Prezzo;
+
+            ctx.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
         public IActionResult Privacy()
         {
             return View();
