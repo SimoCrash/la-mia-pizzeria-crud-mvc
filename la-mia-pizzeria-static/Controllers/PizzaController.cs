6395d9606f24d095c07475c0761919d9a3bc5649
SimoCrash <simone.crescenzi@hotmail.com>
2023-04-13 18:16:06 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,60 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            using var ctx = new PizzeriaContext();
+            var pizzas = ctx.Pizzas.ToArray();
+            return View(pizzas);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            using var ctx = new PizzeriaContext();
+            var pizza = ctx.Pizzas.SingleOrDefault(p => p.Id == id);
+
+            if(pizza == null)
+            {
+                return NotFound($"Non è stato trovato l'id n° {id}");
+            }
+
+            return View(pizza);
+        }
+
+        public IActionResult Create()
+        {
+            var pizza = new Pizza
+            {
+                Foto = "https://picsum.photos/200/300",
+            };
+            return View(pizza);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(pizza);
+            }
+            
+            using var ctx = new PizzeriaContext();
+
+            ctx.Pizzas.Add(pizza);
+            ctx.SaveChanges(); //Attenzione dà problemi dopo UpdatesPizzaInModel e database update se non inserisci l'img
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,26 @@
+﻿
+using la_mia_pizzeria_static.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Inserisci il Nome della pizza che vuoi aggiungere.")]
+        [StringLength(100, ErrorMessage = "Puoi inserire fino a un massimo di 100 caratteri")]
+        [ParoleMultiple(5)]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Inserisci la Descrizione della pizza che vuoi aggiungere.")]
+        public string Descrizione { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Inserisci una foto della pizza che vuoi aggiungere.")]
+        public string? Foto { get; set; }
+
+        [Required(ErrorMessage = "Inserisci il Prezzo della pizza che vuoi aggiungere.")]
+        [Range(1, 30, ErrorMessage = "Inserisci il Prezzo della pizza che vuoi aggiungere, superiore a 1.")]
+        public string Prezzo { get; set; } = string.Empty;
+    }
+}
